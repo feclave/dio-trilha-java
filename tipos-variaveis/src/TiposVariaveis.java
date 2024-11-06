49efb33c11320e982178c2473580e646fe2afb69
Felipe <felipeclave@gmail.com>
2024-11-06 11:50:19 -0300

    inserção de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Felipe Jost Clavé"
+        
+        //tipos primitivos
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; //se começar com zero deve ser uma string
+        long cpf = 98765432109L; //sempre termina com "L" e se começar com zero deve ser uma string
+        float pi = 3.14F; //sempre termina com "F"
+        double salarioMinimo = 2500;
+
+        final double VALOR_DE_PI = 3.14; //(final) constante sempre com nome em caixa alta
     }
 }
